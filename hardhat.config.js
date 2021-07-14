37ba84c9d946a43762de6312cdbf28d258fe424c
Adam Skrodzki <adamskrodzki@gmail.com>
2021-07-14 12:44:02 +0200

    more flexible hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("@tenderly/hardhat-tenderly");
