ae1f4c4f604ce68b9180917e29a3840c1f44b877
Adam Skrodzki <adamskrodzki@gmail.com>
2021-08-06 09:30:29 +0200

    Merge fixes

--- contracts/multiply/MultiplyProxyActions.sol
+++ contracts/multiply/MultiplyProxyActions.sol
@@ -820,12 +820,10 @@ contract MultiplyProxyActions {
     uint256 borrowedDaiAmount = amounts[0].add(premiums[0]);
     emit FLData(IERC20(DAI).balanceOf(address(this)).sub(cdpData.depositDai), borrowedDaiAmount);
 
-    require(cdpData.requiredDebt == amounts[0], "Invalid declared amount");
     require(
       IERC20(DAI).balanceOf(address(this)) == amounts[0],
       "Declared and actual amount mismatch"
     );
-    require(amounts[0] + premiums[0] == borrowedDaiAmount, "Flash loan fee mismatch");
 
     require(
       cdpData.requiredDebt == IERC20(DAI).balanceOf(address(this)),
