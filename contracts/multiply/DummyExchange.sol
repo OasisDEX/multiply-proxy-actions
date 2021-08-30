be7372fbdc676c9fe6d3ac248690225d19028cbc
John <bulk.yard@protonmail.com>
2021-08-30 13:47:17 +0200

    Add WBTC and LINK funds to the dummy exchange and testing wallet

--- contracts/multiply/DummyExchange.sol
+++ contracts/multiply/DummyExchange.sol
@@ -19,7 +19,7 @@ contract DummyExchange {
   uint256 public feeBase = 10000;
 
   address public feeBeneficiaryAddress = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; // second HH address
-  
+
   event AssetSwap(
     address indexed assetIn,
     address indexed assetOut,
