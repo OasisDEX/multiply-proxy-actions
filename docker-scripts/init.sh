2d0411a4d8221d209ad0677ee1fe6a2297d14a6c
John <bulk.yard@protonmail.com>
2021-09-02 16:37:36 +0200

    Main process terminates background process when killed

--- docker-scripts/init.sh
+++ docker-scripts/init.sh
@@ -6,6 +6,11 @@ npx hardhat node --max-memory 8192 &
 # Wait a little bit to be sure the node is started
 sleep 2
 
+# This captures the terminate signal ( Ctrl ^ C ) 
+# and terminates any process started from this shell
+trap "exit" INT TERM ERR
+trap "jobs -p | xargs -r kill" EXIT
+
 # Deploy the system contracts
 npx hardhat run scripts/deploy-system.js --network local
 
