94aeb570c4ce78f3c5f7fc2a60f411fa7144ba7f
Adam Skrodzki <adam@oazoapps.com>
2022-02-02 08:27:44 +0100

    split contracts

--- contracts/multiply_alternative/MakerTools.sol
+++ contracts/multiply_alternative/MakerTools.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.1;
+import "./MakerCalculations.sol";
 import "./interfaces/ExternalInterfaces.sol";
 import "./../utils/SafeMath.sol";
 import "./../interfaces/IERC20.sol";
@@ -9,37 +10,14 @@ import "./../interfaces/mcd/IJoin.sol";
 import "./../../contracts/interfaces/exchange/IExchange.sol";
 
 //TODO:This eventually should be library not inheritance
-contract MakerTools {
+contract MakerTools is MakerCalculations {
   address public immutable daijoin;
   IERC20 public immutable DAI;
-  uint256 constant RAY = 10**27;
   using SafeMath for uint256;
 
   constructor(address _daiJoin, address _dai) {
     daijoin = _daiJoin;
     DAI = IERC20(_dai);
-  }
-
-  function _getWipeDart(
-    address vat,
-    uint256 dai,
-    address urn,
-    bytes32 ilk
-  ) internal view returns (int256 dart) {
-    // Gets actual rate from the vat
-    (, uint256 rate, , , ) = IVat(vat).ilks(ilk);
-    // Gets actual art value of the urn
-    (, uint256 art) = IVat(vat).urns(ilk, urn);
-
-    // Uses the whole dai balance in the vat to reduce the debt
-    dart = toInt256(dai / rate);
-    // Checks the calculated dart is not higher than urn.art (total debt), otherwise uses its value
-    dart = uint256(dart) <= art ? -dart : -toInt256(art);
-  }
-
-  function toInt256(uint256 x) internal pure returns (int256 y) {
-    y = int256(x);
-    require(y >= 0, "int256-overflow");
   }
 
   function wipeAndFreeGem(
@@ -64,32 +42,4 @@ contract MakerTools {
     IJoin(gemJoin).exit(address(this), collateralDraw);
   }
 
-  function convertTo18(address gemJoin, uint256 amt) internal view returns (uint256 wad) {
-    // For those collaterals that have less than 18 decimals precision we need to do the conversion before passing to frob function
-    // Adapters will automatically handle the difference of precision
-    wad = amt.mul(10**(18 - IJoin(gemJoin).dec()));
-  }
-
-  /**
-  TODO: This is great candidate for operation or some library
-   */
-  function _getWipeAllWad(
-    address vat,
-    address usr,
-    address urn,
-    bytes32 ilk
-  ) internal view returns (uint256 wad) {
-    // Gets actual rate from the vat
-    (, uint256 rate, , , ) = IVat(vat).ilks(ilk);
-    // Gets actual art value of the urn
-    (, uint256 art) = IVat(vat).urns(ilk, urn);
-    // Gets actual dai amount in the urn
-    uint256 dai = IVat(vat).dai(usr);
-
-    uint256 rad = art.mul(rate).sub(dai);
-    wad = rad / RAY;
-
-    // If the rad precision has some dust, it will need to request for 1 extra wad wei
-    wad = wad.mul(RAY) < rad ? wad + 1 : wad;
-  }
 }
